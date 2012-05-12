e0d8cb499e534a4d6f1db5b10d496d581e903d24
Tim Cinel <email@timcinel.com>
2012-05-12 12:06:05 +1000

    Adjusted test_Sensor to accommodate Sensor's new design.

--- test/test_Sensor.cpp
+++ test/test_Sensor.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <iostream>
 #include "../Sensor.h"
 
@@ -5,12 +6,28 @@
 int main(void) {
     Controller *mock = new Controller();
 
+    int pipe_fds[2];
+    int write_fds[26];
+
     for (int i = 0; i < 26; i++) {
+
+        if (pipe(pipe_fds) != 0) {
+            std::cout << "Failed to create pipe. Bailing!\n";
+            exit(1);
+        }
+
+        write_fds[i] = pipe_fds[1];
+
         std::cout << "Creating sensor #" << i << "\n";
-        Sensor *sensor = new Sensor(mock, 'a' + i, i);
+        Sensor *sensor = new Sensor(mock, 'a' + i, i, pipe_fds[0]);
     }
 
-    while(true)
-        sleep(5);
+    char buff;
+    while (read(STDIN_FILENO, &buff, 1) > 0) {
+        for (int i = 0; i < 26; i++)
+            if (write(write_fds[i], &buff, 1) != 1) {
+                std::cout << "Failed to write to pipe. Bailing!\n";
+            }
+    }
 
 }
