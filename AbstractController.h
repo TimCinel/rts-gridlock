15a27de6dfad0c25eb9fa56dc5807ce9a09f03a1
Tim Cinel <email@timcinel.com>
2012-05-26 15:33:19 +1000

    Cleaned up states and flags.

--- AbstractController.h
+++ AbstractController.h
@@ -67,12 +67,59 @@
 #define EW_STRAIGHT_F_EW 000100010
 /*#define EW_STRAIGHT_F_F_EW 000000000*/
 
+namespace ControllerStates {
 /*state format: NSCar|NSTram|NSPed|EWCar|EWCarTurning|EWPed*/
 /*state values: R - Red, A - Amber, G - Green, F - Flashing (Amber for Car, Red for Ped), C - Cleared (not on), W - White (tram only)*/
-enum state {STARTUP, STARTUP_F, NS_CLEAR, NS_STRAIGHT, EW_CLEAR, EW_STRAIGHT, OTHER_FLASH,
-            NS_TRAM_G_NS, NS_TRAM_F_NS, NS_STRAIGHT_G_PED_G_NS, NS_STRAIGHT_G_PED_F_NS, NS_STRAIGHT_G_PED_F_F_NS, NS_STRAIGHTG_NS, NS_STRAIGHT_F_NS, EW_STRAIGHT_G_PED_G_NS, EW_STRAIGHT_G_PED_F_NS, EW_BOTH_RIGHT_G_NS, EW_BOTH_RIGHT_F_NS, EW_STRAIGHT_G_NS, EW_STRAIGHT_F_NS,
-            NS_TRAM_G_EW, NS_TRAM_F_EW, NS_STRAIGHT_G_PED_G_EW, NS_STRAIGHT_G_PED_F_EW, NS_STRAIGHT_G_EW, EW_STRAIGHT_F_EW, EW_STRAIGHT_G_PED_G_EW, EW_STRAIGHT_G_PED_F_EW, EW_STRAIGHT_G_PED_F_F_EW, EW_BOTH_RIGHT_G_EW, EW_BOTH_RIGHT_F_EW, EW_STRAIGHT_G_EW, EW_STRAIGHT_F_EW};
-typedef state State;
+    typedef enum controllerState {
+        STARTUP,
+        NS_CLEAR,
+        NS_TRAM_G,
+        NS_TRAM_F,
+        NS_STRAIGHT,
+        NS_STRAIGHT_G_PED_G,
+        NS_STRAIGHT_G_PED_F,
+        NS_STRAIGHT_G_PED_F,
+        NS_STRAIGHT_G,
+        NS_STRAIGHT_F,
+        EW_CLEAR,
+        EW_BOTH_RIGHT_G,
+        EW_BOTH_RIGHT_F,
+        EW_STRAIGHT,
+        EW_STRAIGHT_G_PED_G,
+        EW_STRAIGHT_G_PED_F,
+        EW_STRAIGHT_G,
+        EW_STRAIGHT_F,
+        CONTROLLER_STATE_SENTINAL
+    };
+};
+
+namespace ControllerFlags {
+    typedef enum {
+        SYSTEM_MODE,
+        CMD_EW_STRAIGHT,
+        CMD_EW_PED,
+        CMD_EW_RIGHT,
+        CMD_NS_STRAIGHT,
+        CMD_NS_PED, 
+        CMD_TRAM, 
+        SEN_EW_STRAIGHT,
+        SEN_EW_PED,
+        SEN_EW_RIGHT,
+        SEN_NS_STRAIGHT,
+        SEN_NS_PED, 
+        SEN_TRAM, 
+        SEQ_EW_STRAIGHT,
+        SEQ_EW_PED,
+        SEQ_EW_RIGHT,
+        SEQ_NS_STRAIGHT,
+        SEQ_NS_PED, 
+        SEQ_TRAM, 
+        CONTROLLER_FLAG_SENTINEL,
+        COMMAND_MODE,
+        SENSOR_MODE,
+        TIMER_MODE
+    }
+}
 
 class AbstractController
 {
@@ -82,6 +129,7 @@ class AbstractController
 
    /*functions*/
    public:
+      virtual void trigger() = 0;
       State nextState();
       State getState();
       void tick();
