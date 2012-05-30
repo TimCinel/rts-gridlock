8a2e0d3cd57882a42c6c7041e7be06ae747d04fa
Tim Cinel <email@timcinel.com>
2012-05-31 09:57:31 +1000

    Work on CentralController

--- CentralController.cpp
+++ CentralController.cpp
@@ -1,2 +1,28 @@
 #include "CentralController.h"
 
+void CentralController::trigger()
+{
+    if (this->getTime() > 0)
+        return;
+
+    if ((this->stateRecord[this->state]) != NULL)
+        (this->*stateRecord[this->state])();
+    else
+        (this->*stateRecord[STARTUP])();
+
+}
+
+void CentralController::clearFlag(unsigned int flag) 
+{
+
+}
+
+void CentralController::setFlag(unsigned int flag)
+{
+
+}
+
+int CentralController::getFlag(unsigned int flag)
+{
+
+}
--- CentralController.h
+++ CentralController.h
@@ -1,22 +1,43 @@
-#ifndef CENTRALCONTROLLER
-#define CENTRALCONTROLLER
+#ifndef CENTRALCONTROLLER_H
+#define CENTRALCONTROLLER_H
 
 #include "AbstractController.h"
+#include "IntersectionRecord.h"
+
+namespace CentralInfo {
+
+    typedef enum {
+        INITIALISE,
+        LISTEN,
+        CHANGE_MODE,
+        CENTRAL_STATES_SENTINEL
+    } CENTRAL_STATES;
+}
 
 class CentralController : public AbstractController
 {
-   /*constructor*/
-   public:
-      CentralController();
-      ~virtual CentralController();
+public:
+    //constructor
+    CentralController();
+    virtual ~CentralController();
 
-   /*functions*/
-   public:
-      
+    //functions
+    virtual void trigger() = 0;
+    void tick();
 
-   /*instance variables*/
-   private:
-      
+    virtual void clearFlag(unsigned int flag) = 0;
+    virtual void setFlag(unsigned int flag) = 0;
+    virtual int getFlag(unsigned int flag) = 0;
+
+private:
+    //instance variables
+
+    unsigned int flags[ControllerInfo::CONTROLLER_FLAG_SENTINEL];
+    void (CentralController::*stateRecord
+            [CentralInfo::CENTRAL_STATES_SENTINEL])();
+
+    vector<IntersectionRecord> 
+
 };
 
 #endif
